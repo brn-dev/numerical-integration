00504f989d1e38d700e7756d0331b2a7691133f9
Dominik Baron <dominik.b4ron@gmail.com>
2022-08-12 16:26:10 +0200

    Add custom parameters to main.py

--- numerical-integration/main.py
+++ numerical-integration/main.py
@@ -4,30 +4,31 @@ from integration_rules.trapezoid_rule import trapezoid_rule
 from integration_rules.barrel_rule import barrel_rule
 from numerical_integration import NumericalIntegration
 
-N_SLICES = 1000
-FROM = 2
-TO = 2
-
-def f(x):
-    return 14*x**3
-
 def print_rule_result(rule_name: str, result: float):
     print(f'{rule_name}: {result}')
 
 def main():
+    function_str = input('Function with parameter "x": ')
+    x_from = int(input('From: '))
+    x_to = int(input('To: '))
+    n_slices = int(input('# Slices: '))
+
+    def f(x):
+        return eval(function_str)
+
     print_rule_result(
         'Rectangle rule',
-        NumericalIntegration(rectangle_rule_midpoint).approximate_integration(f, FROM, TO, N_SLICES)
+        NumericalIntegration(rectangle_rule_midpoint).approximate_integration(f, x_from, x_to, n_slices)
     )
 
     print_rule_result(
         'Trapezoid rule',
-        NumericalIntegration(trapezoid_rule).approximate_integration(f, FROM, TO, N_SLICES)
+        NumericalIntegration(trapezoid_rule).approximate_integration(f, x_from, x_to, n_slices)
     )
 
     print_rule_result(
         'Barrel Rule',
-        NumericalIntegration(barrel_rule).approximate_integration(f, FROM, TO, N_SLICES)
+        NumericalIntegration(barrel_rule).approximate_integration(f, x_from, x_to, n_slices)
     )
 
 
